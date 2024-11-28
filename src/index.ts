1ac2fede277d126ed22cf735adbfa54189b4383f
sergiovalbuena <iosergio.valbuena@gmail.com>
2024-11-28 04:11:53 -0800

    add checkout, success, and cancel routes; handle Stripe checkout session creation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { HTTPException } from "hono/http-exception";
 import Stripe from "stripe";
 import "dotenv/config";
 
@@ -7,12 +8,34 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("GET Hono!");
+app.get("/success", (c) => {
+  return c.text("GET Hono Success!");
 });
 
-app.post("/", (c) => {
-  return c.text("POST Hono!");
+app.get("/cancel", (c) => {
+  return c.text("GET Hono Cancel!");
+});
+
+app.post("/checkout", async (c) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: "price_1QQ6RaDyLN7EDSvrHouU2xQl",
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000//cancel",
+    });
+
+    return c.json(session);
+  } catch (error: any) {
+    console.error(error);
+    throw new HTTPException(500, { message: error?.message });
+  }
 });
 
 const port = 3000;
